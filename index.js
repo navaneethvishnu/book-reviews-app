9b2efcf9782b7661a8865773c8f432f7a99784c8
Navaneeth <navaneethvishnu2000@gmail.com>
2025-10-23 16:07:23 +0530

    chore(env): integrate dotenv and move sensitive credentials to .env

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(
   session({
-    secret: "TOPSECRETWORD",
+    secret: process.env.JWT_SECRET,
     resave: false,
     saveUninitialized: true,
   })
@@ -25,11 +27,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "bookreview",
-  password: "1328",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
